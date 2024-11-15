4a8b1d801e7283e2b47f6b237205904b776b77ba
williamtaraldsson <william.taraldsson@skola.taby.se>
2024-11-15 09:57:26 +0100

    Kod

---
+++ merskit.py
@@ -0,0 +1,12 @@
+if x == 1: #Här slumpas det fram monster i rummet
+    
+
+elif x == 2: 
+        print("Det är en kista")
+        
+else: #Här slumpas det fram fälla i rummet
+        print("Det är en fälla")
+        
+    
+    level = level + 1 
+    return level
--- skit.py
+++ skit.py
@@ -1,8 +1,57 @@
-print("skit")
-print("Jag älskar Oliver!")
-print("hej2")
+import random
+
+input("Tryck på enter för att fortsätta")
+
+print("Välkommen till Adventure with William")
+print("Välj vilken karaktär du vill spela som")
+class karaktar:
+    def __init__ (self, ras, namn, hp, styrka):
+        self.ras = ras
+        self.namn = namn
+        self.hp = hp
+        self.styrka = styrka
+
+    def print_karaktar_info(self):
+        print(self.ras, self.namn, self.hp, self.styrka)
+
+karak = karaktar("Dvärg", "Oliver", 100, 10)
+karak2 = karaktar("Alv", "William", 100, 5)
+karak3 = karaktar("Tomte", "Elton", 100, 0)
+
+karak.print_karaktar_info()
+karak2.print_karaktar_info()
+karak3.print_karaktar_info()
+
+print(karak, karak2, karak3)
+vald = int(input("1. Dvärg 2. Alv 3. Tomte --> "))
+
+while vald != 1 and vald != 2 and vald != 3:
+    if vald == 1 or vald == 2 or vald == 3:
+        spelare = vald
+    else:
+        print("Det du skrev var fel, välj ett nummer mellan 1 och 3")
+    vald = int(input("1, 2 eller 3 -->"))
+
+spelare = vald
+if spelare == 1:
+    spelare = karak
+elif spelare == 2:
+    spelare = karak2
+elif spelare == 3:
+    spealre = karak3
+
+def monster_rum(ras, namn, hp, styrka):
+    print("hej")
 
 
-print("ska vi ha prov idag")
+def kista_rum(ras, namn, hp, styrka):
+    print("hej")
 
-https://prod.liveshare.vsengsaas.visualstudio.com/join?273D445C6CE0B728FECEC1989BD4EB083744
+
+def fälla_rum(ras, namn, hp, styrka):
+    print(9)
+
+    
+room_selection_number = random.randint(1,3)
+
+
