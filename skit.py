6c7f1502c2d8b638d7f9ff35af339f14cd23a7c8
williamtaraldsson <william.taraldsson@skola.taby.se>
2024-11-21 10:08:57 +0100

    yas

--- skit.py
+++ skit.py
@@ -2,8 +2,9 @@ import random
 
 input("Tryck på enter för att fortsätta")
 
-print("Välkommen till Adventure with William")
-print("Välj vilken karaktär du vill spela som")
+print("""Välkommen till Adventure with William
+      Välj vilken karaktär du vill spela som""")
+
 class karaktar:
     def __init__ (self, ras, namn, hp, styrka):
         self.ras = ras
@@ -28,7 +29,7 @@ while vald != 1 and vald != 2 and vald != 3:
     if vald == 1 or vald == 2 or vald == 3:
         spelare = vald
     else:
-        print("Det du skrev var fel, välj ett nummer mellan 1 och 3")
+        print("Det du skrev var inte ett heltal mellan 1 och 3, välj ett nummer mellan 1 och 3")
     vald = int(input("1, 2 eller 3 -->"))
 
 spelare = vald
@@ -54,3 +55,4 @@ def fälla_rum(ras, namn, hp, styrka):
 room_selection_number = random.randint(1,3)
 
 
+https://prod.liveshare.vsengsaas.visualstudio.com/join?268E7394D4753BBCF009EBB396062F0AB675
