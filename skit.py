8cf4a69e0fc57eccd1dcd20d701fe186cb565b9c
williamtaraldsson <william.taraldsson@skola.taby.se>
2024-11-28 10:02:48 +0100

    länk

--- skit.py
+++ skit.py
@@ -133,40 +133,21 @@ def monster_rum(ras, namn, hp, styrka, level):
         
 
 
-def kista_rum(spelare):
-    
-
-    
+def kista_rum():
+    print("I rummet du valde finns det en kista")
+    print("Du går fram emot kistan och öppnar den")
     global inventory
-
-    print("""I rummet du valde finns det en kista"
-          Du går fram emot kistan och öppnar den""")
     
     loot_items = [
-        ("Kebab", "I kistan fanns det en kebabrulle, den ger dig mer hälsa!"),
+        ("kebab", "I kistan fanns det en kebabrulle, den ger dig mer hälsa!"),
         ("Rustningsdel", "I kistan fanns det en rustningsdel, detta ger dig mer skydd mot dina fiender!"),
         ("Starkare svärd", "I kistan fanns det ett starkare svärd, detta gör mer skada mot dina fiender!"),
-        ("Förstoringsglas", "I kistan finns det ett förstoringsglas, detta kan du använda för att se vad som finns bakom dörrarna!"),
-        (None, "Du öppnar kistan, men den är tom... Otur!"),
-        ("Fälla", "Det är en fälla! Kistan sprutar ut gift och du förlorar hälsa!")
+        ("Förstoringsglas", "I kistan finns det ett förstoringsglas, detta kan du använda för att se vad som finns bakom dörrarna!")
     ]
-
+    
     loot, message = random.choice(loot_items)
     print(message)
     
-
-    if loot is None:
-        print("Det finns inget att lägga till i ditt inventory. Spelet forsätter.")
-        return
-    
-    if loot == "Fälla":
-        spelare.hp -= 10
-        print(f"Du förlorade 10 hälsa! Din nuvarande hälsa är: {spelare.hp}")
-        if spelare.hp <= 0:
-            print("Du har förlorat all din hälsa. Spelet är slut!")
-        return spelare.hp
-    
-
     if len(inventory) >= 5:
         while True:
             print("""Ditt inventory är fullt, vill du slänga något eller gå vidare?
@@ -205,8 +186,6 @@ def kista_rum(spelare):
         print("Du la till " + loot + " i ditt inventory")
         return inventory
 
-
-
     
 
 
@@ -231,3 +210,4 @@ input
 gärning = get_number()
 
 
+https://prod.liveshare.vsengsaas.visualstudio.com/join?1272A86540D93B1152E9DDF65FC904DFE7D3
